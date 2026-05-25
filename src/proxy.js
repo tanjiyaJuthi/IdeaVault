c53d349a1fbc15457db200c1e108a51e9232681d
tanjiyaJuthi <tanjiya098@gmail.com>
2026-05-25 19:33:33 +0600

    added related ideas

--- src/proxy.js
+++ src/proxy.js
@@ -1,36 +1,37 @@
-// import { NextResponse } from "next/server";
-// import { auth } from "./app/lib/auth";
-
-// export async function proxy(request) {
-//     const { pathname } = request.nextUrl;
-
-//     if (pathname.startsWith("/ideas/")) {
-//         const session = await auth.api.getSession({
-//             headers: request.headers,
-//         });
-
-//         if (!session) {
-//             return NextResponse.redirect(
-//                 new URL(`/login?redirect=${pathname}`, request.url)
-//             );
-//         }
-//     }
-
-//     return NextResponse.next();
-// }
-
-// export const config = {
-//     matcher: [
-//         "/ideas/:path*",
-//         "/add-idea",
-//         "/my-ideas",
-//         "/my-interactions",
-//         "/my-profile",
-//     ],
-// };
-
 import { NextResponse } from "next/server";
+import { auth } from "./app/lib/auth";
 
-export function proxy() {
+export async function proxy(request) {
+    const { pathname } = request.nextUrl;
+
+    if (pathname.startsWith("/ideas/")) {
+        const session = await auth.api.getSession({
+            headers: request.headers,
+        });
+
+        if (!session) {
+            return NextResponse.redirect(
+                new URL(`/login?redirect=${pathname}`, request.url)
+            );
+        }
+    }
+
     return NextResponse.next();
 }
+
+export const config = {
+    matcher: [
+        "/ideas/:path*",
+        "/add-idea",
+        "/my-ideas",
+        "/my-interactions",
+        "/my-profile",
+        "/categories",
+    ],
+};
+
+// import { NextResponse } from "next/server";
+
+// export function proxy() {
+//     return NextResponse.next();
+// }
